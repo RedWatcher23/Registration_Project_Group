efb5cc13595450562c5fc64aaaf683145d8d9961
rommelocon <rjayocon06@gmail.com>
2024-12-02 14:12:58 +0800

    add backend function to connect on database EnrollmentDB

--- Registration_Project/Login.Designer.vb
+++ Registration_Project/Login.Designer.vb
@@ -29,23 +29,24 @@ Partial Class Login
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Btn3 = New System.Windows.Forms.Button()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
         Me.SuspendLayout()
         '
         'TextBox1
         '
-        Me.TextBox1.Font = New System.Drawing.Font("Roboto", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TextBox1.Location = New System.Drawing.Point(317, 284)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(283, 27)
+        Me.TextBox1.Size = New System.Drawing.Size(283, 26)
         Me.TextBox1.TabIndex = 0
         Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'TextBox2
         '
-        Me.TextBox2.Font = New System.Drawing.Font("Roboto", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.TextBox2.Location = New System.Drawing.Point(317, 332)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(283, 27)
+        Me.TextBox2.Size = New System.Drawing.Size(283, 26)
         Me.TextBox2.TabIndex = 1
         Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
@@ -54,13 +55,13 @@ Partial Class Login
         Me.Label1.AutoSize = True
         Me.Label1.BackColor = System.Drawing.Color.White
         Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Label1.Font = New System.Drawing.Font("Open Sans ExtraBold", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.SteelBlue
         Me.Label1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.Label1.Location = New System.Drawing.Point(305, 212)
         Me.Label1.Name = "Label1"
         Me.Label1.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.Label1.Size = New System.Drawing.Size(275, 69)
+        Me.Label1.Size = New System.Drawing.Size(279, 55)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "WELCOME"
         '
@@ -69,20 +70,20 @@ Partial Class Login
         Me.Label2.AutoSize = True
         Me.Label2.BackColor = System.Drawing.Color.White
         Me.Label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Label2.Font = New System.Drawing.Font("Open Sans", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.Color.SteelBlue
         Me.Label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.Label2.Location = New System.Drawing.Point(311, 190)
         Me.Label2.Name = "Label2"
         Me.Label2.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.Label2.Size = New System.Drawing.Size(97, 35)
+        Me.Label2.Size = New System.Drawing.Size(97, 29)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "HELLO,"
         '
         'Btn3
         '
         Me.Btn3.BackColor = System.Drawing.Color.SteelBlue
-        Me.Btn3.Font = New System.Drawing.Font("Roboto", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btn3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Btn3.ForeColor = System.Drawing.SystemColors.ButtonHighlight
         Me.Btn3.Location = New System.Drawing.Point(469, 390)
         Me.Btn3.Name = "Btn3"
@@ -94,7 +95,7 @@ Partial Class Login
         'Button1
         '
         Me.Button1.BackColor = System.Drawing.Color.SteelBlue
-        Me.Button1.Font = New System.Drawing.Font("Roboto", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight
         Me.Button1.Location = New System.Drawing.Point(352, 390)
         Me.Button1.Name = "Button1"
@@ -103,12 +104,23 @@ Partial Class Login
         Me.Button1.Text = "LOGIN"
         Me.Button1.UseVisualStyleBackColor = False
         '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(445, 442)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(46, 13)
+        Me.LinkLabel1.TabIndex = 8
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Register"
+        '
         'Login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.ClientSize = New System.Drawing.Size(1264, 681)
+        Me.Controls.Add(Me.LinkLabel1)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Btn3)
         Me.Controls.Add(Me.Label2)
@@ -128,4 +140,5 @@ Partial Class Login
     Friend WithEvents Label2 As Label
     Friend WithEvents Btn3 As Button
     Friend WithEvents Button1 As Button
+    Friend WithEvents LinkLabel1 As LinkLabel
 End Class
---
+++ Registration_Project/Register.vb
@@ -0,0 +1,86 @@
+﻿Imports System.Configuration
+Imports System.Data.SqlClient
+Imports System.Security.Cryptography
+Imports System.Text
+
+Public Class Register
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim connectionString As String = ConfigurationManager.ConnectionStrings("EnrollmentDB").ConnectionString
+
+        Dim username As String = txtUsername.Text.Trim()
+        Dim password As String = txtPassword.Text.Trim()
+        Dim confirmPassword As String = txtConfirmPassword.Text.Trim()
+        Dim email As String = txtEmail.Text.Trim()
+        Dim fullName As String = txtFullName.Text.Trim()
+        Dim userType As String = cmbUserType.SelectedItem?.ToString()
+
+        ' Validate inputs
+        If String.IsNullOrEmpty(username) OrElse String.IsNullOrEmpty(password) OrElse
+           String.IsNullOrEmpty(confirmPassword) OrElse String.IsNullOrEmpty(email) OrElse
+           String.IsNullOrEmpty(fullName) OrElse String.IsNullOrEmpty(userType) Then
+            MessageBox.Show("All fields are required.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        If password <> confirmPassword Then
+            MessageBox.Show("Passwords do not match.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Hash the password
+        Dim hashedPassword As String = HashPassword(password)
+
+        ' Save to database
+        Try
+            Using conn As New SqlConnection(connectionString)
+                conn.Open()
+                Dim query As String = "INSERT INTO Users (Username, Password, Email, FullName, UserType, IsActive, CreatedAt) VALUES (@Username, @Password, @Email, @FullName, @UserType, @IsActive, @CreatedAt)"
+                Using cmd As New SqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@Username", username)
+                    cmd.Parameters.AddWithValue("@Password", hashedPassword)
+                    cmd.Parameters.AddWithValue("@Email", email)
+                    cmd.Parameters.AddWithValue("@FullName", fullName)
+                    cmd.Parameters.AddWithValue("@UserType", userType)
+                    cmd.Parameters.AddWithValue("@IsActive", 1)
+                    cmd.Parameters.AddWithValue("@CreatedAt", DateTime.Now)
+
+                    Dim rowsAffected = cmd.ExecuteNonQuery()
+                    If rowsAffected > 0 Then
+                        MessageBox.Show("Registration successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                        Me.Close() ' Optionally close the form
+                    Else
+                        MessageBox.Show("Registration failed.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End If
+                End Using
+            End Using
+        Catch ex As Exception
+            MessageBox.Show($"Error: {ex.Message}", "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Function HashPassword(plainTextPassword As String) As String
+        Using sha256 As SHA256 = sha256.Create()
+            Dim hashedBytes As Byte() = sha256.ComputeHash(Encoding.UTF8.GetBytes(plainTextPassword))
+            Return Convert.ToBase64String(hashedBytes)
+        End Using
+    End Function
+
+    Private Sub Register_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim connectionString As String = ConfigurationManager.ConnectionStrings("EnrollmentDB").ConnectionString
+
+        ' Query to fetch user types from the database
+        Dim query As String = "SELECT UserTypeId, UserTypeDescription FROM UserTypes"
+
+        Using conn As New SqlConnection(connectionString)
+            conn.Open()
+            Using cmd As New SqlCommand(query, conn)
+                Using reader As SqlDataReader = cmd.ExecuteReader()
+                    While reader.Read()
+                        ' Add each user type to the ComboBox
+                        cmbUserType.Items.Add(reader("UserTypeDescription").ToString())
+                    End While
+                End Using
+            End Using
+        End Using
+    End Sub
+End Class
